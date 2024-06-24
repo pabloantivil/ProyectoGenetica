e23ece77066ed0bb419431cf57bbb4243dab6420
Platano <bcarrasco2022@alu.uct.cl>
2024-06-23 20:57:21 -0400

    Co-authored-by: JaviCuri <JaviCuri@users.noreply.github.com> Co-authored-by: Coto2412 <Coto2412@users.noreply.github.com> Co-authored-by: Pablo Antivil <pabloantivil@users.noreply.github.com> Co-authored-by: Carlos Riquelme <SrCarlito@users.noreply.github.com>

--- nano.py
+++ nano.py
@@ -4,11 +4,11 @@ import numpy as np
 NPob = 10 
 SEED = 12
 NCicles = 2
-POBLACION = []
+Poblacion = []
 MAP = [[]]
 nGen = 10
 nPasos = 10
-
+Asesinatos = 0
 
 class individuo:
     def __init__(self,genes=[],position=[0,0]) -> None:
@@ -18,8 +18,8 @@ class individuo:
         self.position = position
         # self asesino (true o false)
 
-        self.direcciones = {0: 'n', 1: 's', 2: 'e', 3: 'o', 4: 'ne', 5: 'no', 6: 'se', 7: 'so', 8: 'asesino'}
         # [n, s, e, o, ne, no, se, so, asesino]
+        # [1, 1, 0, 1, 0 , 1,   0,  1,     0  ]
         #self.gen()
 
         if len(self.genes) == 0:
@@ -30,7 +30,7 @@ class individuo:
     #javier
     def generar_genes(self):
         #generear los genes de el individuo
-        genes = self.normalizar([np.random.uniform() for _ in range(8)])
+        genes = self.normalizar([np.random.uniform() for _ in range(9)])
         return genes
     
     #javier 
@@ -55,12 +55,9 @@ class individuo:
     
     #coto
     def move(self):
-        #muevete CTM
-        direccion = self.direcciones[np.random.choice(9, p=self.genes)]
-        nueva_pos = posicionar(self.position, direccion)
-        #retornar poss
-        pass
-
+        mov = np.random.randint(len(self.genes))
+        if self.genes[mov] == 1 :
+            self.position=posicionar(mov , self.position, self.genes[8])
 
 
 # pablo 
@@ -88,52 +85,81 @@ def cruce(c1, c2):
 #benja
 def fitness():
     #Se ordena de Menor a mayor segun los pasos que dan 
-    POBLACION.sort(key=lambda sujeto: sujeto.pasos)
+    Poblacion.sort(key=lambda sujeto: sujeto.pasos)
 
 #pablo Funcion de seleccion que devuelve los dos mejores individuos por su fitness 
-def seleccion(poblacion_ordenada):
-    c1 = poblacion_ordenada[0]
-    c2 = poblacion_ordenada[1]
+def seleccion(Poblacion_ordenada):
+    c1 = Poblacion_ordenada[0]
+    c2 = Poblacion_ordenada[1]
     return c1, c2
 
 #benja
-def rellenar_poblacion(): 
-    dif = -NPob - len(POBLACION) 
+def rellenar_Poblacion(): 
+    dif = -NPob - len(Poblacion) 
     if dif > 0 :
         for i in range(dif):
-            POBLACION.append(individuo.generar_genes())
+            Poblacion.append(individuo.generar_genes())
 
 #carlo
-def init_poblacion():
-    #generar poblacion
-    pass
+def init_Poblacion():
+    for x in range(NPob):
+        i = individuo(position=[x,0])
+        Poblacion.append(i)
+        MAP[x][0] = i
+    
 
-                                     
+def colision(pos):
+    index_indi = 0
+    for a in Poblacion:
+        if a.position[0] == pos[0] and a.position[1] == pos[1]:
+            return True , index_indi  
+        index_indi +=1                          
+    return False
 
 # coto
-def posicionar(posicion, direccion, valorX, valorY):
-    # Función para actualizar la posición según la dirección
-    if direccion == 'n':
-        return (posicion[0], max(0, posicion[1] - 1))
-    elif direccion == 's':
-        return (posicion[0], min(valorY, posicion[1] + 1))
-    elif direccion == 'e':
-        return (min(valorX, posicion[0] + 1), posicion[1])
-    elif direccion == 'o':
-        return (max(0, posicion[0] - 1), posicion[1])
-    elif direccion == 'ne':
-        return (min(valorX, posicion[0] + 1), max(0, posicion[1] - 1))
-    elif direccion == 'no':
-        return (max(0, posicion[0] - 1), max(0, posicion[1] - 1))
-    elif direccion == 'se':
-        return (min(valorX, posicion[0] + 1), min(valorY, posicion[1] + 1))
-    elif direccion == 'so':
-        return (max(0, posicion[0] - 1), min(valorY, posicion[1] + 1))
-    elif direccion == 'asesino':
-        return posicion
+"""MAP = [[0,X,0,0],
+          [0,0,0,0],
+          [0,0,0,0],
+          [0,0,0,0]]
+"""
 
+def posicionar(posm,posicion,a):
+        # [n, s, e, o, ne, no, se, so, asesino]
+    if posm == 0 :
+        #n
+        posicion[0]-=1
+    elif posm == 1:
+        #s
+        posicion[0]+=1
+    elif posm == 2:
+        #e
+        posicion[1]+=1   
+    elif posm == 3:
+        #o
+        posicion[1]-=1
+    elif posm == 4:
+        #ne
+        posicion[1]+=1 ; posicion[0]-=1
+    elif posm == 5:
+        #no
+        posicion[0]-=1 ; posicion[1]-=1
+    elif posm == 6:
+        #se
+        posicion[0]+=1 ; posicion[1]+=1
+    elif posm == 7:
+        #so
+        posicion[0]+=1 ; posicion[0]-=1; 
+    
+    if posicion[0]>=0 and posicion[1]>=0 and posicion[0]<=len(MAP) and posicion[1] <= len(MAP):
+        colisiono , indexcol = colision(posicion)
+        if colisiono:
+            if a == 1:
+                Poblacion.pop(indexcol)
+                Asesinatos +=1
+                return posicion
+        else :
+            return posicion
 
-#
 def main():
     gen = 1
     while(gen <= nGen ):
